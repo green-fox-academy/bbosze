eee83ca5f5c3f28ae845bf9d596f9beca0821e3e
bbosze <b.bosze@gmail.com>
2018-02-20 16:50:00 +0100

    student-counter almost ready

--- week-02/day2/student-counter.ts
+++ week-02/day2/student-counter.ts
@@ -10,32 +10,28 @@ let students: any[] = [
   {name: 'Gerzson', age: 10, candies: 1},
 ];
 
-// create a function that takes a list of students and logs:
+
 // - how many candies are owned by students
-// create a function that takes a list of students and logs:
-// - Sum of the age of people who have lass than 5 candies
-
-
-//console.log(Object.keys(students));
-// console.log(students.length);
-
-
 let numberOfCandies: number = 0;
-console.log(students.candies);
 
-/*for (let i: number = 0; i < students.length; i++) {
-  numberOfCandies = students.candies[i];
-
+function candyCounter() {
+  for (let i: number = 0; i < students.length; i++) {
+    numberOfCandies += students[i].candies;
+  }
 }
-*/
+candyCounter();
+console.log(numberOfCandies);
 
+//Sum of the age of people who have lass than 5 candies
+let poorChildrenAge: number = 0;
 
-//console.log(numberOfCandies);
-
-//console.log(Object.values(students));
-
-/*
-function candies (candies: number) {
-
+function poorChildren() {
+  for (let i: number = 0; i < students.length; i++) {
+    if (students[i].candies < 5) {
+      poorChildrenAge += students[i].age;
+    }
+  }
 }
-*/
+
+poorChildren();
+console.log(poorChildrenAge);
--- week-02/day2/takes-longer.ts
+++ week-02/day2/takes-longer.ts
@@ -4,7 +4,11 @@
 // Add "always takes longer than" to between the words "It" and "you"
 let quote: string = 'Hofstadter\'s Law: It you expect, even when you take into account Hofstadter\'s Law.'
 
+//With replace
+// quote = quote.replace(/you expect, even when/, 'always takes longer than');
 
-quote = quote.replace(/you expect, even when/, 'always takes longer than');
+//With slice
+quote = quote.slice(0, 21) + 'always takes longer than' + quote.slice(46)
+
 
 console.log(quote);
