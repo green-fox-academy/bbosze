86d276c2da6e9b3ab81969b424328ddc971ad3e7
bbosze <b.bosze@gmail.com>
2018-02-22 12:00:55 +0100

    checkerboard with array input

--- week-02/day3/checkerboard-ts.ts/checkerboard-ts.ts
+++ week-02/day3/checkerboard-ts.ts/checkerboard-ts.ts
@@ -35,44 +35,47 @@ drawSquare(20, 'red');
 
 //2 COLOURED CHECKERBOARD
 
-function drawSquare(size: number, firstRowcolor: string, secondRowcolor: string) {
-  let x: number = size;
-  let y: number = size;
+let checkerBoard:[number, string, string] = [20, 'red', 'gold'];
+
+
+function drawSquare(checkerBoardData: any[]) {
+  let x: number = checkerBoardData[0];
+  let y: number = checkerBoardData[0];
   let z: number = 0;
   for (let j:number = 0; j < canvas.height/y; j++) {
       if (j % 2 === 0) {
-      x = size;
-      z = size * (j + 1);
+      x = checkerBoardData[0];
+      z = checkerBoardData[0] * (j + 1);
       for (let i:number = 0; i < canvas.width/y/2; i++) {
       z = j * y+1
-      ctx.fillStyle = firstRowcolor;
+      ctx.fillStyle = checkerBoardData[1];
       ctx.fillRect(x, z, y, y);
       x += y * 2
      }} else  {
      x = 0;
-     z = size * (j);
+     z = checkerBoardData[0] * (j);
      for (let i:number = 0; i < canvas.width/y/2; i++) {
-     ctx.fillStyle = firstRowcolor
+     ctx.fillStyle = checkerBoardData[1]
      ctx.fillRect(x, z, y, y);
      x += y * 2;
 }}}
   for (let j:number = 0; j < canvas.height/y; j++) {
     if (j % 2 === 0)     {
      x = 0;
-     z = size * (j);
+     z = checkerBoardData[0] * (j);
      for (let i:number = 0; i < canvas.width/y/2; i++) {
-     ctx.fillStyle = secondRowcolor;
+     ctx.fillStyle = checkerBoardData[2];
      ctx.fillRect(x, z, y, y);
      x += y * 2;
   }}
     else {
-      x = size;
-      z = size * (j);
+      x = checkerBoardData[0];
+      z = checkerBoardData[0] * (j);
       for (let i:number = 0; i < canvas.width/y/2; i++) {
-      ctx.fillStyle = secondRowcolor;
+      ctx.fillStyle = checkerBoardData[2];
       ctx.fillRect(x, z, y, y);
       x += y * 2;
     }
 } } }
 
-drawSquare(20, 'red', 'gold');
+drawSquare(checkerBoard);
