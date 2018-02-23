ef09e4dfc89bcb0443549a2e3b1cad0715e80123
bbosze <b.bosze@gmail.com>
2018-02-23 12:59:49 +0100

    line play OK

---
+++ week-02/day5/line-play/line-play.ts
@@ -0,0 +1,38 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// create a line drawing function that takes 2 parameters:
+// The x and y coordinates of the line's starting point
+// and draws a line from that point to the center of the canvas.
+// Fill the canvas with lines from the edges, every 20 px, to the center.
+
+
+
+let num:number = 50;
+let w:number = canvas.width;
+let h:number = canvas.height;
+
+
+for (let i:number = 0; i < num; i++) {
+  let x:number = i * w / (num - 1);
+  let y:number = i * h / (num - 1);
+  drawLine(0, y, x, h);
+}
+
+for (let i:number = 0; i < num; i++) {
+  let x:number = i * w / (num - 1);
+  let y:number = i * h / (num - 1);
+  drawLine(w, y, x, 0);
+}
+
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.strokeStyle = 'red';
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+}
