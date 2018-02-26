dc46e35df5e2178e93eb0cd5ac7efe7d51055694
bbosze <b.bosze@gmail.com>
2018-02-26 18:50:23 +0100

    he will never finally finished, line play with perfect prompt checker

--- week-02/day2/he-will-never.ts
+++ week-02/day2/he-will-never.ts
@@ -2,9 +2,8 @@
 // Things are a little bit messed up
 // Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
 // Assemble the fragments into the out variable
-let out: string = '';
-let notSoCrypticMessage: number[] = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11];
 
+let notSoCrypticMessage: number[] = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11];
 let hashmap = {
   7: 'run around and desert you',
   50: 'tell a lie and hurt you ',
@@ -16,15 +15,17 @@ let hashmap = {
   3: 'say goodbye '
 };
 
-console.log()
-
-let outArray: string[] = [];
-
-
-function coder (arr:number[], obj) {
-    for (let i: number=0; i< arr.length; i++) {
-      console.log(Object.values(obj.arr[i]));
+function coder(code, map) {
+  let newArray: any[] =[];
+  let hashmapArray: any[] = Object.entries(map);
+  for (let i:number = 0; i < code.length; i++) {
+    for (let j: number = 0; j < hashmapArray.length; j++) {
+      if (code[i] == hashmapArray[j][0]) {
+          newArray.push(hashmapArray[j][1])
+      }
     }
-   }
+  }
+console.log(newArray.join(''));
+}
 
 coder(notSoCrypticMessage, hashmap);
--- week-02/day5/line-play-quarters/line-play-quarters.ts
+++ week-02/day5/line-play-quarters/line-play-quarters.ts
@@ -19,9 +19,9 @@ function draw(num, tiles) {
     for (let j:number = 0; j < tiles; j++) {
     for (let i:number = 0; i < num; i++) {
     let x:number = j * w / tiles + i * w / (num - 1) / tiles;
-    let y:number = i * h / tiles / (num - 1) + k * h / tiles;
+    let y:number = k * h / tiles + i * h / tiles / (num - 1);
     drawLine(w / tiles * j, y, x, h / tiles * (k + 1));
-    drawLine(w / tiles * (j+1), y, x, h / tiles * k);
+    drawLine(w / tiles * (j + 1), y, x, h / tiles * k);
 }}}}
 
 function drawLine(x1, y1, x2, y2) {
@@ -33,22 +33,28 @@ function drawLine(x1, y1, x2, y2) {
 }
 
 //PROMPT CHECKING function
-let numberOfLines: number = parseInt(prompt('PLease enter the number of the lines'));
-if (numberOfLines > 50) {
-    numberOfLines = parseInt(prompt("It is too big. Please enter a number from 1 to 50", ""));
-} else if (isNaN(numberOfLines)) {
-    numberOfLines = parseInt(prompt("It is not a number. Please enter a number from 1 to 100", ""));
-} else {
-}
 
-let numberOfTiles: number = parseInt(prompt('PLease enter the number of the tiles'));
-if (numberOfTiles > 16) {
-    numberOfTiles = parseInt(prompt("It is too big. Please enter a number from 1 to 50", ""));
-} else if (isNaN(numberOfTiles)) {
-    numberOfTiles = parseInt(prompt("It is not a number. Please enter a number from 1 to 100", ""));
-} else {
+function checker(range: any, range2: any) {
+  let numberOfLines: any = parseInt(prompt('Please enter the number of the lines (between 0-' + range + ')'));
+  if (numberOfLines > range) {
+      alert('This is too big!')
+      checker(range, range2);
+  } else if (isNaN(numberOfLines)) {
+      alert('This is not a number!')
+      checker(range, range2);
+  } else {
+    numberOfLines = numberOfLines
+    let numberOfTiles: any = parseInt(prompt('Please enter the number of the tiles (between 0-' + range2  + ')'));
+    if (numberOfTiles > range2) {
+        alert('This is too big!')
+        checker(range, range2);
+    } else if (isNaN(numberOfTiles)) {
+        alert('This is not a number!')
+        checker(range, range2);
+    } else {
+      numberOfTiles = numberOfTiles
+      draw(numberOfLines, numberOfTiles);
+  }}}
 
-}
 
-
-draw(numberOfLines, numberOfTiles);
+checker(40, 20);
