96d4bffe5a1e7b134915dd53c36d67fcb220791b
bbosze <b.bosze@gmail.com>
2018-03-01 17:31:36 +0100

    fleet of things completed

--- week-03/day3/fleet-of-things/flettOfThings.ts
+++ week-03/day3/fleet-of-things/flettOfThings.ts
@@ -13,11 +13,11 @@ class Thing {
         this.completed = true;
     }
 
-    getName() {
+    get names() {
       return this.name;
     }
 
-    getComplete() {
+    get completer() {
       return this.completed;
     }
 }
@@ -35,40 +35,34 @@ class Fleet {
 
     printAll() {
       for (let i:number = 0; i < this.things.length; i++){
-        if (fleet[i].getComplete()) {
-          console.log(fleet[i]);
+        if (this.things[i].completer) {
+          console.log('[ ] ' + this.things[i].names);
           }
+         else {
+           console.log('[X] ' + this.things[i].names);
+         }
         }
       }
     }
-
 
 let fleet = new Fleet();
 
 let thing1 = new Thing('Get milk');
 let thing2 = new Thing('Remove obstacles');
 let thing3 = new Thing('Stand up');
+let thing4 = new Thing('Eat lunch');
 
+thing2.complete()
+thing4.complete()
 
-thing3.complete()
+// console.log(thing1);
+// console.log(thing3)
 
-
-console.log(thing1);
-console.log(thing3)
-
-fleet.add(thing1);
+fleet.add(thing1); //OR fleet.add(new Thing('Eat lunch'));
 fleet.add(thing2);
 fleet.add(thing3);
-fleet.add(new Thing('Eat lunch'));
+fleet.add(thing4);
+
+
 
 fleet.printAll();
-
-//fleet.print(0);
-//fleet.printAll();
-
-//* crete a fleet of things to have this output:*/
-//* 1. [ ] Get milk */
-//* 2. [ ] Remove the obstacles */
-//* 3. [x] Stand up */
-//* 4. [x] Eat lunch */
-//* Hint: You have to create a Print method also */
