e209bca8776383ed97c62134e00643798aebb377
bbosze <b.bosze@gmail.com>
2018-03-07 14:17:51 +0100

    zoo.ts

---
+++ week-04/zoo/zoo.ts
@@ -0,0 +1,51 @@
+'use strict'
+
+abstract class Animal {
+  name: string;
+  age: number;
+  gender: string;
+  hunger: number = 50;
+  tiredness: number = 50;
+
+  constructor(name) {
+    this.name = name
+  }
+  getName(){
+    return this.name
+  }
+
+  abstract breed()
+
+  sleep() {
+  }
+}
+
+class Mammal extends Animal {
+  breed() {
+    return 'valahogy'
+  }
+
+}
+
+class Reptile extends Animal {
+  breed() {
+    return 'mint egy gyík'
+  }
+
+}
+
+class Bird extends Animal {
+  breed() {
+    return 'mint egy madár'
+  }
+
+}
+
+let reptile = new Reptile("Crocodile");
+let mammal = new Mammal("Koala");
+let bird = new Bird("Parrot");
+
+console.log("How do you breed?");
+console.log("A " + reptile.getName() + " is breeding by " + reptile.breed());
+console.log("A " + mammal.getName() + " is breeding by " + mammal.breed());
+console.log("A " + bird.getName() + " is breeding by " + bird.breed());
