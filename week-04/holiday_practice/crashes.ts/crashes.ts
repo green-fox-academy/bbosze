d8c28d3c62307b317bd06c1cae669c3a8f2e966e
bbosze <b.bosze@gmail.com>
2018-03-14 16:26:19 +0100

    crash statistics OK

---
+++ week-04/holiday_practice/crashes.ts/crashes.ts
@@ -0,0 +1,23 @@
+'use strict'
+const fs = require('fs');
+
+
+
+function weatherCheck() {
+  let splittedArray: any[] = fs.readFileSync('crash-incidents.txt', 'utf-8').split(';');
+
+  let goodWeatherCrashes: number = 0;
+  let badWeatherCrashes: number = 0;
+  for (let i: number = 5; i < splittedArray.length; i += 7) {
+    if (splittedArray[i] === 'CLOUDY' || splittedArray[i] === 'CLEAR') {
+      goodWeatherCrashes += 1
+    }
+    else if (splittedArray[i] === 'RAIN' || splittedArray[i] === 'FREEZING RAIN' || splittedArray[i] === 'SNOW' || splittedArray[i] === 'FOG' || splittedArray[i] === 'SEVERE CROSSWINDS') {
+      badWeatherCrashes += 1
+    }
+  }
+  return [goodWeatherCrashes, badWeatherCrashes]
+}
+
+let result: number[] = weatherCheck()
+console.log(result[0], result[1]);
