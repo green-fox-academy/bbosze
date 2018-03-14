54ecedc0d5ede4e4a92d89003321ecae3f77aa48
bbosze <b.bosze@gmail.com>
2018-03-14 17:21:03 +0100

    corsac retake exam finished

---
+++ week-04/corsac-exam/fishtank/fishtank.ts
@@ -0,0 +1,97 @@
+'use strict'
+
+class Aquarium {
+  fishes: any[] = [];
+
+  add(newFish: Fish) {
+    this.fishes.push(newFish);
+  }
+
+  feedAllFishes() {
+    for (let i: number = 0; i < this.fishes.length; i++) {
+      this.fishes[i].feed();
+    }
+  }
+
+  status() {
+    for (let i: number = 0; i < this.fishes.length; i++) {
+      console.log(this.fishes[i].status);
+    }
+  }
+
+  removeOversizedFishes() {
+    for (let i: number = 0; i < this.fishes.length; i++) {
+      if (this.fishes[i].weight >= 11) {
+        this.fishes.splice(i, 1)
+      }
+    }
+  }
+}
+
+class Fish {
+  name: string;
+  weight: number;
+  color: string;
+
+  constructor(name: string, weight: number, color: string) {
+    this.name = name;
+    this.weight = weight;
+    this.color = color;
+  }
+
+  feed() {
+    this.weight += 1;
+  }
+
+  get status() {
+    return `${this.name}, weigth: ${this.weight}, color: ${this.color}.`
+  }
+
+}
+
+class Clownfish extends Fish {
+  stripecolor: string;
+  constructor(name: string, weight: number, color: string, stripecolor: string ) {
+    super(name, weight, color)
+    this.stripecolor = stripecolor;
+  }
+  get status() {
+    return `${this.name}, weigth: ${this.weight}, color: ${this.color}, stripe color: ${this.stripecolor}.`
+  }
+}
+
+class Tang extends Fish {
+  shortMemory: boolean;
+
+  constructor(name: string, weight: number, color: string, shortMemory: boolean) {
+    super(name, weight, color);
+    this.shortMemory = shortMemory;
+  }
+
+  get status() {
+    return `${this.name}, weigth: ${this.weight}, color: ${this.color}, short-term memory loss: ${this.shortMemory}.`
+  }
+
+}
+
+class Koi extends Fish {
+  constructor(name, weight, color) {
+    super(name, weight, color)
+  }
+
+  feed() {
+    this.weight += 2;
+  }
+}
+
+const aquarium: Aquarium = new Aquarium();
+
+aquarium.add(new Koi('Nami', 9, 'pink'));
+aquarium.add(new Tang('Dory', 8, 'blue', true));
+aquarium.add(new Tang('Bubbles', 10, 'yellow', false));
+aquarium.add(new Clownfish('Nemo', 5, 'orange', 'white'));
+
+aquarium.status();
+aquarium.feedAllFishes();
+aquarium.removeOversizedFishes();
+aquarium.status();
