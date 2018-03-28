34da8c267808b6cbf3b2efcb92d48fae4be7bf1c
bbosze <b.bosze@gmail.com>
2018-03-28 14:32:04 +0200

    attribute-1 finished

--- week-06/day3/content-1/content-1.js
+++ week-06/day3/content-1/content-1.js
@@ -3,10 +3,10 @@ let lastPcontent = document.querySelector('.animals').innerHTML;
 let pContent = document.querySelectorAll('p');
 
 
-// for (let i = 0; i < pContent.length; i++) {
-//   pContent[i].innerHTML = lastPcontent;
-// }
+for (let i = 0; i < pContent.length; i++) {
+  pContent[i].innerHTML = lastPcontent;
+}
 
-pContent.forEach(lastPcontent => {
-  pContent.innerHTML = lastPcontent;
-});
+// pContent.forEach(lastPcontent => {
+//   pContent.innerHTML = lastPcontent;
+// });
