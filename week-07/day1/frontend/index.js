393d18efe322667cd49e71650a39f4c300f2a745
bbosze <b.bosze@gmail.com>
2018-04-03 14:18:27 +0200

    doubling OK

--- week-07/day1/frontend/index.js
+++ week-07/day1/frontend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const PORT = 3000;
+const PORT = 8080;
 
 app.use('/static', express.static('static'));
 app.use('/assets', express.static('assets'));
@@ -16,3 +16,25 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
+
+app.get('/doubling', (req, res) => {
+  const number = req.query.input;
+  if (number != undefined) {
+    res.json({
+    "received": number,
+    "result": number * 2
+    });
+  }
+    res.json({
+      "error": "Please provide an input!"
+    })
+  });
+
+
+// app.get('/api/hello', (req, res) => {
+//   console.log(req.query);
+//   const name = req.query.name || 'Guest';
+//   res.json({
+//     message: `Hello ${name}`,
+//   });
+// });
