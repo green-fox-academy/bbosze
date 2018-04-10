97dab249a03f829e24817e071835829e7dc3b781
bbosze <b.bosze@gmail.com>
2018-04-10 13:28:52 +0200

    multiplier OK

---
+++ week-08/day2/multiplier/multiplier.js
@@ -0,0 +1,29 @@
+// Create the multiplier function that you can use like this:
+
+
+function multiplier (multiplierNum) {
+  return function(number) {
+    return multiplierNum * number
+  };
+}
+
+
+console.log(multiplier);
+
+const duplicator = multiplier(2);
+
+console.log(duplicator(5)); // should log 10
+console.log(duplicator(10)); // should log 20
+
+const threeTimes = multiplier(3);
+
+console.log(threeTimes(5)); // should log 15
+console.log(threeTimes(100)); // should log 300
+
+
+let multiplyWithBind = (number, multiplier) => {
+  return multiplier * number
+}
+
+let duplicateWithBind = multiplyWithBind.bind(this, 2);
+console.log(duplicateWithBind(10));
