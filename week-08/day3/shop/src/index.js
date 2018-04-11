ab6b2d21991ce08baa8ebeec68c311c9185e2c65
bbosze <b.bosze@gmail.com>
2018-04-11 19:43:40 +0200

    css inserted

--- week-08/day3/shop/src/index.js
+++ week-08/day3/shop/src/index.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const path = require('path'); // not necessary
+const mysql = require('mysql');
 
 const app = express();
 const PORT = 3000;
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'x',
+  database: 'shop',
+  // port: 3306,
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // not necessary
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
-  res.send('Hello')
+  const sql = 'SELECT * FROM history ORDER BY date DESC LIMIT 15;';
+  conn.query(sql, (err, result) => {
+    if (err) {
+      console.log(err); // eslint-disable-line
+      res.sendStatus(500);
+    }
+    res.render('index', {
+      title: 'Dashboard',
+      history: result,
+    });
+  });
 });
 
 app.listen(PORT, () => {
-  console.log(`App is running on ${PORT}`); // eslint-disable-
+  console.log(`App is running on ${PORT}`); // eslint-disable-line
 });
