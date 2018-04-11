0faab486c64c62c581b076393f26edd1f43c83ff
bbosze <b.bosze@gmail.com>
2018-04-11 15:53:33 +0200

    new york times OK

---
+++ week-08/day3/newyork-times/script.js
@@ -0,0 +1,29 @@
+let article = document.querySelector('article');
+
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=948c2dad2ec84c1dabdf9ef298c47fab&q=apollo&fq=moon
+`);
+xhr.onload = () => {
+  let dataFromNyt = JSON.parse(xhr.responseText);
+  console.log(dataFromNyt.response.docs[0]);
+  dataFromNyt.response.docs.forEach((e) => {
+    let container = document.createElement('div');
+    article.appendChild(container);
+    let newH1 = document.createElement('h1');
+    container.appendChild(newH1);
+    newH1.textContent = e.headline.main
+    let newP = document.createElement('p');
+    container.appendChild(newP);
+    newP.textContent = e.snippet
+    let newDate = document.createElement('p');
+    container.appendChild(newDate);
+    e.pub_date = e.pub_date.substring(0, 16).replace('T', ' ')
+    newDate.textContent = e.pub_date;
+    let newA = document.createElement('a');
+    container.appendChild(newA);
+    newA.textContent = 'Read full article'
+    newA.href = e.web_url
+  })
+};
+xhr.send();
